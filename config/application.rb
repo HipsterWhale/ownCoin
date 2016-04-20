0553d482d1d5ae31c3624b87e2df0024c6ab3837
Jérémy SEBAN <jeremy@seban.eu>
2016-04-20 11:07:38 +0200

    Prepared login system

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,14 @@ Bundler.require(*Rails.groups)
 
 module OwnCoin
   class Application < Rails::Application
+
     config.autoload_paths << Rails.root.join('lib')
+
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
