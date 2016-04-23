8cd72481f17ba16dbb69e9e13323e49498b7c4a0
Jérémy SEBAN <jeremy@seban.eu>
2016-04-23 20:15:08 +0200

    Fixed blockr api call

--- lib/blockr_btcapi.rb
+++ lib/blockr_btcapi.rb
@@ -1,29 +1,23 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class BlockrBtcApi
 
-  def initialize
-    @cached_rate = nil
-  end
+  attr_accessor :cached_rate
 
   def btc_to_eur(wanted_btc)
     current_rate = exchange_rate
-    btc = current_rate['data']['rates']['BTC'].to_f
-    eur = current_rate['data']['rates']['EUR'].to_f
+    btc = current_rate['data'][0]['rates']['BTC'].to_f
+    eur = current_rate['data'][0]['rates']['EUR'].to_f
     ((wanted_btc * eur) / btc).round(2)
   end
 
   private
 
     def exchange_rate
-      if @cached_rate and @cached_rate[:expire_date].future?
-        @cached_rate[:current_rate]
-      else
-        current_rate = ask_api URI.parse('http://btc.blockr.rb.io/api/v1/exchangerate/current')
-        @cached_rate = {
-            expire_date: DateTime.now + 5.minutes,
-            current_rate: current_rate
-        }
-        current_rate
-      end
+      @cached_rate = ask_api(URI.parse('http://btc.blockr.io/api/v1/exchangerate/current')) unless @cached_rate
+      @cached_rate
     end
 
     def ask_api(uri)
